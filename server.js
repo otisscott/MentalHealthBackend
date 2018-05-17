75ec64f5a8a0319c2a1e36fdf5b7f6db16de6f49
Otis Scott <otismscott@gmail.com>
2018-05-17 10:18:57 -0700

    added daniels stupid stuff

--- server.js
+++ server.js
@@ -45,9 +45,10 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', *);
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 // Mental Health App Backend Stuff
