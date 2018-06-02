0974763b2c3451f4156974be72875f71747a935e
danielmartinezkrams <danielmartinezkrams@students.berkeley.net>
2018-06-01 17:18:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongodb = require("mongodb");
 const csv = require("fast-csv");
 const ObjectID = mongodb.ObjectID;
-//const twilio = require("twilio");
+const twilio = require("twilio");
 
 //MentalHealth Collections
 const MENTALHEALTHUSERS = "mentalhealthusers";
