5bfb8bcb825d9f517363fa2a4279be0a690bf864
DanielMartinez-Krams <danielmartinezkrams@gmail.com>
2018-06-01 18:11:05 -0700

    new

--- server.js
+++ server.js
@@ -370,6 +370,7 @@ app.post("/berkeleyeats/api/send", (req, res) => {
     const client = new twilio(SID, TOKEN);
     const text = req.body;
     text.createDate = new Date();
+
     text.note = "new";
   
    client.messages.create({
